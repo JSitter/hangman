3707973bce04e43b5f1d8cdd2529db8d5da1c2f1
Justin Sitter <justin@jaytria.com>
2017-09-26 12:20:46 -0700

    Added win game ability

--- hangman.py
+++ hangman.py
@@ -11,7 +11,6 @@ def loadWord():
    secretWord = random.choice(wordsList)
    return secretWord
 
-
 def isWordGuessed(secretWord, lettersGuessed):
     '''
     secretWord: string, the random word the user is trying to guess.  This is selected on line 9.
@@ -19,10 +18,12 @@ def isWordGuessed(secretWord, lettersGuessed):
     returns: boolean, True only if all the letters of secretWord are in lettersGuessed;
       False otherwise
     '''
-    return False
-
-
-
+    #print(set(secretWord))
+    #check if the intersection of sets is equal in length to the length of the set of secret word
+    if len(set(secretWord).intersection(set(lettersGuessed))) == len(set(secretWord)):
+        return True
+    else:
+        return False
 
 def getGuessedWord(secretWord, lettersGuessed):
     '''
@@ -46,28 +47,29 @@ def getAvailableLetters(lettersGuessed):
 def checkGuess(currentGuess, previousGuesses):
     #check if guess is a letter
     if not currentGuess.isalpha():
-        print("Hey! Only letters are valid in this game.")
-        return False
+        return "Hey! Only letters are valid in this game."
 
-    if(currentGuess in previousGuesses):
-        print("You already guessed that letter brah.")
-        return False
+    if currentGuess in previousGuesses:
+        return "You already guessed that letter brah."
 
     if len(currentGuess) > 1:
-        print("So which letter is it? Only one guess at a time allowed. \n\nTRY AGAIN!")
-        return False
-
-    if(len(currentGuess) == 1):
-        return True
-
-    print("I literally can't even right now. Try again.")
-    return False
+        return "So which letter is it? Only one guess at a time allowed. \n\nTRY AGAIN!"
     
+    #correct user input return no errors
+    if len(currentGuess) == 1:
+        return ""
+
+    #Catch all for unforseen circumstances
+    return "I literally can't even right now. Try again."
 
 #Output game data to player
 #All inputs must be strings
-def displayGameState(board, guessedLetters, remainingGuesses):
-    print("Makeschool Deathmatch Hangman!\n\n\nYou have " + str(remainingGuesses) + " remaining.\t\tCurrent Guesses: " + str(guessedLetters) + "\n\n" + board, end='\r')
+def displayGameState(board, guessedLetters, remainingGuesses, feedback = ""):
+    os.system("clear")
+    print("Makeschool Deathmatch Hangman!\n\n")
+    print("You have " + str(remainingGuesses) + " remaining.\t\tCurrent Guesses: " + str(guessedLetters))
+    print("\n\n" + board)
+    print(feedback)
 
 #convert game data to output string
 def drawBoard(secretWord, guessedLetters):
@@ -79,10 +81,11 @@ def drawBoard(secretWord, guessedLetters):
             board = board + " _ "
     return board
 
-def winScreen():
+def winScreen( word ):
     os.system("clear")
     print("Has anyone told you how smart you are?")
     print("***You WON!****")
+    print("The secret word was: ", word)
     print("Now is a good time to enter your wizarding skillz into hangman tournament deathmatch.")
 
 
@@ -105,9 +108,10 @@ def hangman(secretWord):
       partially guessed word so far, as well as letters that the
       user has not yet guessed
     '''
-    #get terminal width and height
+    #get terminal width and height for no reason at the moment :(
     rows, columns = os.popen('stty size', 'r').read().split()
     os.system("clear")
+
     #flag to indicate whether player is in game
     ingame = True
 
@@ -131,46 +135,54 @@ def hangman(secretWord):
 
     #Flag for game win
     gameWin = False
-
+    user_feedback = ""
     while(ingame):
         if newGame:
             #Initialize New Game
             secretWord = loadWord()
             newGame = False
         if not gameOver:
-            displayGameState(drawBoard(secretWord, guesses), guesses, str(total_guesses-guessNum))
-            newGuess = input("\n\n\nGuess a letter: ")
+            displayGameState(drawBoard(secretWord, guesses), guesses, str(total_guesses-guessNum), user_feedback)
+            user_feedback = ""
+            newGuess = input("\n\nGuess a letter: ")
             #Verify user input to continue
-            if(checkGuess(newGuess, guesses)):
-                guesses = guesses + newGuess
-                guessNum = guessNum + 1
+            user_feedback = checkGuess(newGuess, guesses)
 
-                #trigger end of game
-                if(guessNum <= total_guesses):
-                    if(isWordGuessed(secretWord, guesses)):
-                        #set game win
-                        gameOver == True
-                        gameWin == True
-                else:
+            #If there are no errors continue
+            if not user_feedback:
+                guesses = guesses + newGuess
+                
+                gameWin = isWordGuessed(secretWord, guesses)
+                #check if guess is incorrect
+                if newGuess not in secretWord:
+                    guessNum = guessNum + 1
+
+                if gameWin:
                     gameOver = True
+                    winScreen( secretWord )
                     
+                #trigger end of game loss
+                if guessNum == total_guesses:
+                    gameOver = True
+
         else:
             #Game Over
             #Check for Win
             if(gameWin):
                 #Display you win game screen
-                winScreen()
+                winScreen(secretWord)
             else:
                 print("You're DEAD!")
             replay = input("Would you like to play again? y or n: ") 
             if(replay == 'y'):
                 #reset board
                 board = ""
-                guesses = ''
+                guesses = ""
                 guessNum = 0
                 newGame = True
                 gameOver = False
                 gameWin = False
-
+            else:
+                ingame = False
 
 hangman(loadWord())
